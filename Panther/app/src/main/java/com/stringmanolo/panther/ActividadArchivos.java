204eeadf865908fa969f9b66642e5cd27d2235f3
StringManolo <manuelvarelacaldas@gmail.com>
2024-07-15 16:58:04 +0000

    Ident ActividadArchivos.java

--- Panther/app/src/main/java/com/stringmanolo/panther/ActividadArchivos.java
+++ Panther/app/src/main/java/com/stringmanolo/panther/ActividadArchivos.java
@@ -2,29 +2,19 @@ package com.stringmanolo.panther;
 
 import com.stringmanolo.panther.R;
 
-
 import android.os.Bundle;
 import android.app.Activity;
-
 import android.view.View;
 import android.widget.ListView;
 
-public class ActividadArchivos extends Activity
-{
+public class ActividadArchivos extends Activity {
+  private ListView ListaArchivos;
 
-   private ListView ListaArchivos;
-
-
-   @Override
-   protected void onCreate(Bundle savedInstanceState)
-   {
-   super.onCreate(savedInstanceState);
-   setContentView(R.layout.layout_archivos); 
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.layout_archivos); 
    
-   ListaArchivos = (ListView) findViewById(R.id.ListaArchivos);
-   
-   
-   
-   
-   }
+    ListaArchivos = (ListView) findViewById(R.id.ListaArchivos);
+  }
 }
