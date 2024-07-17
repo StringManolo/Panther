4e7943e0fdef0a8fa7e70a13fa9033dc51b4649a
StringManolo <manuelvarelacaldas@gmail.com>
2024-07-17 05:41:30 +0000

    Add some visual changes

--- Panther/app/build/generated/source/com/stringmanolo/panther/R.java
+++ Panther/app/build/generated/source/com/stringmanolo/panther/R.java
@@ -32,11 +32,12 @@ public final class R {
         public static final int barrabusquedaurl=0x7f020000;
         public static final int barraedittext=0x7f020001;
         public static final int barraedittext2=0x7f020002;
-        public static final int fondo_menu_icono=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
-        public static final int panthera=0x7f020005;
-        public static final int panthera2=0x7f020006;
-        public static final int patair=0x7f020007;
+        public static final int consoleborder=0x7f020003;
+        public static final int fondo_menu_icono=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int panthera=0x7f020006;
+        public static final int panthera2=0x7f020007;
+        public static final int patair=0x7f020008;
     }
     public static final class id {
         public static final int ListaArchivos=0x7f07000b;
